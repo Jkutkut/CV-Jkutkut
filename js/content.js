f97e316f20b1001a9ac6d43bb9cd95f351379cdd
jkutkut <jorgeregonzalez@gmail.com>
2020-09-28 18:44:40 +0200

    filling with info

--- js/content.js
+++ js/content.js
@@ -6,11 +6,20 @@ window.onload = function(){
 }
 
 function updateContent(json){
+   console.log("executing update content");
+   console.log(json);
    content = json;
+   // TOP-BAR
    for(let i = 0; i < json["top-bar"]["fields"][lanIndex].length; i++){
-      let href = json["top-bar"]["href"][lanIndex][i];
+      let href = json["top-bar"]["href"][i];
       let label = json["top-bar"]["fields"][lanIndex][i];
-      let ele = $("<li class=\"nav-item\"><a href=\"" + href + "\" class=\"nav-link\"><span>" + label + "</span></a></li>")
-      $("#topBar").append(ele)
+      let ele = $("<li class=\"nav-item\"><a href=\"" + href + "\" class=\"nav-link\"><span>" + label + "</span></a></li>");
+      $("#topBar").append(ele);
    }
+
+   // INTRO
+   
+   // BIO
+   console.log("Execution ended");
+
 }
