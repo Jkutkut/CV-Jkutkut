4041a6bae2072a507ba8a405e6d956bd030d4fa1
jkutkut <jorgeregonzalez@gmail.com>
2020-09-28 15:14:27 +0200

    files created to improve performance

--- js/content.js
+++ js/content.js
@@ -1,4 +1,10 @@
+var lanIndex = (navigator.language == "es-ES")? 0 : 1;
+
 
 window.onload = function(){
-    alert("works")
+   fetch("content/content.json").then(response => response.json()).then(json => updateContent(json));
+}
+
+function updateContent(json){
+    
 }
